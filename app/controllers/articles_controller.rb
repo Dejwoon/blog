3a15800c2d1cf264c403e011fd38d6035e42751d
Dejwoon <d.poddolny@gmail.com>
2017-12-02 13:07:01 +0100

    Migrate  Articles to SQL

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
   def index
   end
-  
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    article_params = params.require(:article).permit(:title, :text)
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
   end
 end
