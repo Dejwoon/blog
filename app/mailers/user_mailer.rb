415ad70d1deb00ebf22242aabad1f75e3826d143
Dejwoon <d.poddolny@gmail.com>
2018-01-28 14:23:39 +0100

    add foreman and other things

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def article_commented(article_id)
-    @article = Article.find(article_d)
-    
+    @article = Article.find(article_id)
+
     mail subject: "Artykuł został skomentowany", to: @article.author.email
   end
 end
