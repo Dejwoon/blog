8ad9d60a888650f03f87840da02a173f63afcdb0
Dejwoon <d.poddolny@gmail.com>
2018-01-28 09:58:23 +0100

    debug emials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
   @comment.attributes = comment_params
   if @comment.save
       redirect_to article_path(@comment.article_id)
+      UserMailer.article_commented(Article.first).deliver_now
   else
       @like = Like.find_or_initialize_by(article: @article, user: current_user)
       render 'articles/show'
