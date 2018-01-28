cdc600110cab4f025a4b18cfc710e53d34fb2581
Dejwoon <d.poddolny@gmail.com>
2018-01-28 09:49:01 +0100

    edit mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,6 @@
 class UserMailer < ApplicationMailer
   def article_commented(article)
     @article = article
-
-    mail subject: "Artykuł został skomentowany", @article.author.email
-    
-
+    mail subject: "Artykuł został skomentowany", to: @article.author.email
   end
 end
